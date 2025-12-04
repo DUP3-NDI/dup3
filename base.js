887b59b47e8866cff7da64824938e48df2e99203
Yanis lacluche <kingy9999theking@gmail.com>
2025-12-04 19:26:46 +0100

    commence a devenir fou

--- base.js
+++ base.js
@@ -1,32 +1,25 @@
 var pc = document.getElementById("pc_image");
-pc.addEventListener("mousedown", initialClick, false);
-var moving = false;
-let image = null;
+var image = pc;
+pc.addEventListener("dragstart", e => e.preventDefault());
+pc.addEventListener("mousedown", e => e.preventDefault());
+pc.addEventListener("mousedown", (e)=>{
+    console.log("je suis dans down");
+    
+  document.addEventListener("mousemove", move)
+  document.addEventListener("mouseup", mouseup)
+  });
+function mouseup(e){
+    document.removeEventListener("mousemove", move) 
+
+}
+function move(e){
+    const w = pc.offsetWidth / 2;
+    const h = pc.offsetHeight / 2;
+    var newX = e.clientX -w;
+    var newY = e.clientY -h;
+
+    image.style.left = newX + "px";
+    image.style.top = newY + "px";
+}
 
 console.log("je marche bien");
-initialClick(pc);
-function move(e){
-
-  var newX = e.clientX - 10;
-  var newY = e.clientY - 10;
-
-  image.style.left = newX + "px";
-  image.style.top = newY + "px";
-
-  
-}
-
-function initialClick(e) {
-
-  if(moving){
-    document.removeEventListener("mousemove", move);
-    moving = !moving;
-    return;
-  }
-  
-  moving = !moving;
-  image = this;
-
-  document.addEventListener("mousemove", move, false);
-
-}
