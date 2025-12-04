c8a8fb0c238e09f27c448aa7d03fc457368ec767
Yanis lacluche <kingy9999theking@gmail.com>
2025-12-04 18:34:18 +0100

    modif

--- base.js
+++ base.js
@@ -1,21 +1,32 @@
 var pc = document.getElementById("pc_image");
 pc.addEventListener("mousedown", initialClick, false);
+var moving = false;
+let image = null;
 
+console.log("je marche bien");
+initialClick(pc);
 function move(e){
+
   var newX = e.clientX - 10;
   var newY = e.clientY - 10;
+
   image.style.left = newX + "px";
   image.style.top = newY + "px";
+
+  
 }
 
 function initialClick(e) {
+
   if(moving){
     document.removeEventListener("mousemove", move);
     moving = !moving;
     return;
   }
-
+  
   moving = !moving;
   image = this;
+
   document.addEventListener("mousemove", move, false);
+
 }
